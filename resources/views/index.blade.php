36e9b29c65f12439734e089c1e25c1071cf63d9b
clemenclay <clemenclay@gmail.com>
2018-09-29 12:15:48 -0300

    primer comit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+@section('content')
+<!-- start content -->
+<div id="fh5co-main">
+	<div class="container">
+		<div class="row">
+	        <div id="fh5co-board" data-columns>
+	        	@foreach($propiedades as $q)
+	        	<div class="item">
+	        		<div class="animate-box">
+		        		<a href="{{asset('/')}}{{$q->image}}" class="image-popup fh5co-board-img" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, eos?">
+		        			<img src="{{asset('/')}}{{$q->image}}" alt="">
+		        		</a>
+	        		</div>
+	        		<div class="fh5co-desc">{!! $q->description !!}</div>
+					<div class="fh5co-desc">{{$q->tipopropiedad}}</div>
+					<div class="fh5co-desc">{{ $q->valor }}</div>
+					
+					
+	        	</div>  
+	        	@endforeach	
+	        </div>
+        </div>
+    </div>
+</div>
+<!-- end content -->
+@endsection
