d611f35060b043014c07e8255f4743253f57c244
clemenclay <clemenclay@gmail.com>
2018-10-06 23:30:02 -0300

    htaccess

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/inmobiliaria/public'),
+    'url' => env('APP_URL', 'https://propiedades.bluepixel.com.ar'),
 
     /*
     |--------------------------------------------------------------------------
