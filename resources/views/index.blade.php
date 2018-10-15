c114a61f07c5d96f915d81ad2d472664c969bea2
clemenclay <clemenclay@gmail.com>
2018-10-15 17:07:16 -0300

    template material

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,42 @@
 @extends('layout')
 @section('content')
+
+
+
+
+
+
+
+
+
+
+
 <!-- start content -->
-<div id="fh5co-main">
-	<div class="container">
-		<div class="row">
-	        <div id="fh5co-board" data-columns>
-	        	@foreach($propiedad as $q)
-	        	<div class="item">
-	        		<div class="animate-box">
-		        		<a href="{{asset('/')}}{{$q->imagen}}" class="image-popup fh5co-board-img" title="sad">
-		        			<img src="{{asset('/')}}{{$q->imagen}}" alt="">
-		        		</a>
-	        		</div>
-	        		<div class="fh5co-desc">
-						<h4>{!! $q->titulo !!}</h4>
-						{!! $q->descripcion !!}
-					</div>
-					<div class="fh5co-desc">{{$q->tipopropiedad}}</div>
-					<div class="col-md-offset-4">
-						{{ $q->precio_compra }}{{ $q->precio_alquiler }} <b>{{ $q->moneda }}</b>
-					</div>
-	        	</div>  
+
+
+
+				@foreach($propiedad as $q)
+				
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+						<div class="card">
+								<img class="card-img-top" src="{{asset('/')}}{{$q->imagen}}" alt="">		
+							<div class="body">
+									<div class="fh5co-desc">
+											<h4>{!! $q->titulo !!}</h4>
+											{!! $q->descripcion !!}
+										</div>
+										<div class="fh5co-desc">{{$q->tipopropiedad}}</div>
+										<div class="col-md-offset-4">
+											{{ $q->precio_compra }}{{ $q->precio_alquiler }} <b>{{ $q->moneda }}</b>
+										</div>
+							</div>
+						</div>
+				</div>
+
 	        	@endforeach	
-	        </div>
-        </div>
-    </div>
-</div>
+
+
+
+
 <!-- end content -->
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -41,33 +41,241 @@
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 	<link rel="shortcut icon" href="favicon.ico">
 
-	<!-- Google Webfonts -->
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 	
+
 	<!-- Animate.css -->
-	<link rel="stylesheet" href="{{asset('frontend/css/animate.css')}}">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="{{asset('frontend/css/icomoon.css')}}">
-	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="{{asset('frontend/css/magnific-popup.css')}}">
-	<!-- Salvattore -->
-	<link rel="stylesheet" href="{{asset('frontend/css/salvattore.css')}}">
-	<!-- Theme Style -->
-	<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
-	<!-- Modernizr JS -->
-	<script src="{{asset('frontend/js/modernizr-2.6.2.min.js')}}"></script>
-	<script src='http://tristanedwards.me/u/SweetAlert/lib/sweet-alert.js'></script>
-    <link rel="stylesheet" href='http://tristanedwards.me/u/SweetAlert/lib/sweet-alert.css'> 
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
+	<link rel="stylesheet" href="{{asset('frontend/material/plugins/bootstrap/css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('frontend/material/plugins/node-waves/waves.css')}}">	
+	<link rel="stylesheet" href="{{asset('frontend/material/plugins/animate-css/animate.css')}}">
+	<link rel="stylesheet" href="{{asset('frontend/material/plugins/morrisjs/morris.css')}}">
+	<link rel="stylesheet" href="{{asset('frontend/material/css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('frontend/material/css/themes/all-themes.css')}}">
+	<link rel="stylesheet" href="{{asset('frontend/css/custom.css')}}">
+
 
 	</head>
-	<body>
-		
-	<div id="fh5co-offcanvass">
+	<body class="theme-indigo">
+
+			<nav class="navbar">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
+							<a href="javascript:void(0);" class="bars" style="display: block;"></a>
+							<a class="navbar-brand" href="../../index.html">ADMINBSB - MATERIAL DESIGN</a>
+						</div>
+						<div class="collapse navbar-collapse" id="navbar-collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<!-- Call Search -->
+								<li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
+								<!-- #END# Call Search -->
+								<!-- Notifications -->
+								<li class="dropdown">
+									<a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
+										<i class="material-icons">notifications</i>
+										<span class="label-count">7</span>
+									</a>
+									<ul class="dropdown-menu">
+										<li class="header">NOTIFICATIONS</li>
+										<li class="body">
+											<div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 254px;"><ul class="menu" style="overflow: hidden; width: auto; height: 254px;">
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<div class="icon-circle bg-light-green">
+															<i class="material-icons">person_add</i>
+														</div>
+														<div class="menu-info">
+															<h4>12 new members joined</h4>
+															<p>
+																<i class="material-icons">access_time</i> 14 mins ago
+															</p>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<div class="icon-circle bg-cyan">
+															<i class="material-icons">add_shopping_cart</i>
+														</div>
+														<div class="menu-info">
+															<h4>4 sales made</h4>
+															<p>
+																<i class="material-icons">access_time</i> 22 mins ago
+															</p>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<div class="icon-circle bg-red">
+															<i class="material-icons">delete_forever</i>
+														</div>
+														<div class="menu-info">
+															<h4><b>Nancy Doe</b> deleted account</h4>
+															<p>
+																<i class="material-icons">access_time</i> 3 hours ago
+															</p>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<div class="icon-circle bg-orange">
+															<i class="material-icons">mode_edit</i>
+														</div>
+														<div class="menu-info">
+															<h4><b>Nancy</b> changed name</h4>
+															<p>
+																<i class="material-icons">access_time</i> 2 hours ago
+															</p>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<div class="icon-circle bg-blue-grey">
+															<i class="material-icons">comment</i>
+														</div>
+														<div class="menu-info">
+															<h4><b>John</b> commented your post</h4>
+															<p>
+																<i class="material-icons">access_time</i> 4 hours ago
+															</p>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<div class="icon-circle bg-light-green">
+															<i class="material-icons">cached</i>
+														</div>
+														<div class="menu-info">
+															<h4><b>John</b> updated status</h4>
+															<p>
+																<i class="material-icons">access_time</i> 3 hours ago
+															</p>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<div class="icon-circle bg-purple">
+															<i class="material-icons">settings</i>
+														</div>
+														<div class="menu-info">
+															<h4>Settings updated</h4>
+															<p>
+																<i class="material-icons">access_time</i> Yesterday
+															</p>
+														</div>
+													</a>
+												</li>
+											</ul><div class="slimScrollBar" style="background: rgba(0, 0, 0, 0.5); width: 4px; position: absolute; top: 0px; opacity: 0.4; display: block; border-radius: 0px; z-index: 99; right: 1px;"></div><div class="slimScrollRail" style="width: 4px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 0px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+										</li>
+										<li class="footer">
+											<a href="javascript:void(0);" class=" waves-effect waves-block">View All Notifications</a>
+										</li>
+									</ul>
+								</li>
+								<!-- #END# Notifications -->
+								<!-- Tasks -->
+								<li class="dropdown">
+									<a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
+										<i class="material-icons">flag</i>
+										<span class="label-count">9</span>
+									</a>
+									<ul class="dropdown-menu">
+										<li class="header">TASKS</li>
+										<li class="body">
+											<div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 254px;"><ul class="menu tasks" style="overflow: hidden; width: auto; height: 254px;">
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<h4>
+															Footer display issue
+															<small>32%</small>
+														</h4>
+														<div class="progress">
+															<div class="progress-bar bg-pink" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 32%">
+															</div>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<h4>
+															Make new buttons
+															<small>45%</small>
+														</h4>
+														<div class="progress">
+															<div class="progress-bar bg-cyan" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+															</div>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<h4>
+															Create new dashboard
+															<small>54%</small>
+														</h4>
+														<div class="progress">
+															<div class="progress-bar bg-teal" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 54%">
+															</div>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<h4>
+															Solve transition issue
+															<small>65%</small>
+														</h4>
+														<div class="progress">
+															<div class="progress-bar bg-orange" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 65%">
+															</div>
+														</div>
+													</a>
+												</li>
+												<li>
+													<a href="javascript:void(0);" class=" waves-effect waves-block">
+														<h4>
+															Answer GitHub questions
+															<small>92%</small>
+														</h4>
+														<div class="progress">
+															<div class="progress-bar bg-purple" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 92%">
+															</div>
+														</div>
+													</a>
+												</li>
+											</ul><div class="slimScrollBar" style="background: rgba(0, 0, 0, 0.5); width: 4px; position: absolute; top: 0px; opacity: 0.4; display: block; border-radius: 0px; z-index: 99; right: 1px;"></div><div class="slimScrollRail" style="width: 4px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 0px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+										</li>
+										<li class="footer">
+											<a href="javascript:void(0);" class=" waves-effect waves-block">View All Tasks</a>
+										</li>
+									</ul>
+								</li>
+								<!-- #END# Tasks -->
+								<li class="pull-right"><a href="javascript:void(0);" class="js-right-sidebar" data-close="true"><i class="material-icons">more_vert</i></a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>		
+
+
+
+
+
+
+
+
+
+
+
+
+	<div >
 		<a href="#" class="fh5co-offcanvass-close js-fh5co-offcanvass-close">Menu <i class="icon-cross"></i> </a>
 		<h1 class="fh5co-logo"><a class="navbar-brand" href="index.html">{{$title}}</a></h1>
 		<ul>
@@ -119,22 +327,41 @@
 	</footer>
 
 	<!-- jQuery -->
-	<script src="{{asset('frontend/js/jquery.min.js')}}"></script>
+	<script src="{{asset('frontend/material/plugins/jquery/jquery.min.js')}}"></script>
 	<!-- jQuery Easing -->
-	<script src="{{asset('frontend/js/jquery.easing.1.3.js')}}"></script>
+	<script src="{{asset('frontend/material/plugins/bootstrap/js/bootstrap.js')}}"></script>
 	<!-- Bootstrap -->
-	<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('frontend/material/plugins/bootstrap-select/js/bootstrap-select.js')}}"></script>
 	<!-- Waypoints -->
-	<script src="{{asset('frontend/js/jquery.waypoints.min.js')}}"></script>
+	<script src="{{asset('frontend/material/plugins/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
 	<!-- Magnific Popup -->
-	<script src="{{asset('frontend/js/jquery.magnific-popup.min.js')}}"></script>
+	<script src="{{asset('frontend/material/plugins/node-waves/waves.js')}}"></script>
 	<!-- Salvattore -->
-	<script src="{{asset('frontend/js/salvattore.min.js')}}"></script>
+	<script src="{{asset('frontend/material/plugins/jquery-countto/jquery.countTo.js')}}"></script>
 	<!-- Main JS -->
-	<script src="{{asset('frontend/js/main.js')}}"></script>
-
-	
-
+	<script src="{{asset('frontend/material/plugins/raphael/raphael.min.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/morrisjs/morris.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/chartjs/Chart.bundle.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/flot-charts/jquery.flot.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/flot-charts/jquery.flot.resize.js')}}"></script>	
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/flot-charts/jquery.flot.pie.js')}}"></script>	
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/flot-charts/jquery.flot.categories.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/flot-charts/jquery.flot.time.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/plugins/jquery-sparkline/jquery.sparkline.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/js/admin.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/js/pages/index.js')}}"></script>
+	<!-- Main JS -->
+	<script src="{{asset('frontend/material/js/demo.js')}}"></script>
 	
 	</body>
 </html>
