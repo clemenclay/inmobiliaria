e36c49ffbe58d140534628c2e9551a5e8dfb958d
clemenclay <clemenclay@gmail.com>
2018-10-31 21:32:53 -0300

    dasasd

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,12 @@
 @extends('layout')
 @section('content')
-	<div id="fh5co-main">
-		<div class="container">
+
 			<div class="row">
-				<div class="col l6">
-					<h2>Contacto</h2>
-					<div class="fh5co-spacer fh5co-spacer-sm"></div>
-					<form class="float-label" spellcheck="false" action="{{ action('FrontendController@postContact') }}" method="post">       
+				<div class="col s12 m6 l6">
+					
+					<h5>Contacto</h5>
+					<div class="card hoverable">
+					<form class="contact-form" spellcheck="false" action="{{ action('FrontendController@postContact') }}" method="post">       
 	                <input type="hidden" name="_token" value="{{ csrf_token() }}" />      
 					
 							{{-- <div class="col-md-6">
@@ -15,18 +15,19 @@
 								</div>
 							</div> --}}
 
-								<div class="row">
+								
 										<div class="input-field col s6">
 										<input id="first_name" type="text" class="validate">
 										<label for="first_name">Nombre</label>
 										<span class="helper-text" data-error="Comprete Correctamente" data-success="ok"></span>
 										</div>
+
 										<div class="input-field col s6">
 										<input id="last_name" type="text" class="validate">
 										<label for="last_name">Apellido</label>
 										<span class="helper-text" data-error="Comprete Correctamente" data-success="ok"></span>
 										</div>
-								</div>
+								
 							{{-- <div class="col-md-6">
 								<div class="form-group">
 									<input type="text" class="form-control" placeholder="Last Name" name="last_name">
@@ -37,29 +38,29 @@
 								{{-- <div class="form-group">
 									<input type="email" class="form-control" placeholder="Email Address" name="email" required="">
 								</div> --}}
-								<div class="row">
+								
 									
-									  <div class="row">
+									  
 										<div class="input-field col s12">
 										  <input id="email" type="email" class="validate">
 										  <label for="email">Email</label>
 										  <span class="helper-text" data-error="Ingrese un e-mail Válido" data-success="ok"></span>
 										</div>
-									  </div>
+									  
 									
-								</div>
+								
 
-								<div class="row">
+								
 									
-									  <div class="row">
+									  
 										<div class="input-field col s12">
 										  <textarea name="message" id="message" class="materialize-textarea validate" type="text"></textarea>
 										  <label for="message">Escriba su mensaje aquí</label>
 										  <span class="helper-text" data-error="Comprete Correctamente" data-success="ok"></span>
 										</div>
-									  </div>
+									  
 									
-								</div>
+								
 								{{-- <div class="form-group">
 									<textarea name="message" id="message" cols="30" class="form-control" rows="10" required=""></textarea>
 								</div> --}}
@@ -69,15 +70,20 @@
 							
 				
 					</form>
-					
+				</div>					
 				</div>
-				<div class="col l6">
-						<iframe width="100%" height="250" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=-34.626039,-58.38654&amp;zoom=15&amp;key=AIzaSyAOCzRBIJRykPBCzw_F1eggTD4uCxCwVRg" allowfullscreen="false"></iframe>
+				<div class="col s12 m6 l6">
+					<h5>Ubicación</h5>
+					<div class="card hoverable">
+						
+						<iframe width="100%" height="442" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=-34.626039,-58.38654&amp;zoom=15&amp;key=AIzaSyAOCzRBIJRykPBCzw_F1eggTD4uCxCwVRg" allowfullscreen="false"></iframe>
+						
+					</div>
 				</div>
         		
         	</div>
-       </div>
-	</div>
+
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 @if(Session::has('message'))
